6485230b6a567125ecd397c27b06b5a7ed94c00f
sirhafizho <hafizemail135@gmail.com>
2020-05-31 23:30:47 +0800

    LETAK COMMENT SAHAJA

--- JumpyGrof-github/src/jumpygrof/github/JumpyGrof.java
+++ JumpyGrof-github/src/jumpygrof/github/JumpyGrof.java
@@ -7,6 +7,8 @@ public class JumpyGrof {
     /**
      * @param args the command line arguments
      */
+    
+    //TEST NIBBA
     static LinkedList<Kangaroo> Kangaroos = new LinkedList<Kangaroo>();// Kangaroos static so other class can use it
     static Map Hafiz = new Map();// Map is static so that other class can access it
 
